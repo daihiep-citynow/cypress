28fbd36f107cc45bfbc0e1ae8c76186353d97158
Trương Tấn Thuận <tanthuan.truong@citynow.vn>
2022-09-13 11:14:26 +0700

    Config jest

--- craco.config.js
+++ craco.config.js
@@ -5,22 +5,22 @@ const path = require("path");
 module.exports = () => ({
   webpack: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "src")
     },
     plugins: [
       new WebpackBar({ profile: true }),
       ...(process.env.NODE_ENV === "development"
         ? [new BundleAnalyzerPlugin({ openAnalyzer: false })]
-        : []),
-    ],
+        : [])
+    ]
   },
   // Add jest config resolve alias path
   jest: {
     configure: {
       moduleNameMapper: {
         // resolve @/constants path
-        "^@/(.+)$": "<rootDir>/src/$1",
-      },
-    },
-  },
+        "^@/(.+)$": "<rootDir>/src/$1"
+      }
+    }
+  }
 });
---
+++ src/utils/index.test.js
@@ -0,0 +1,5 @@
+describe("", () => {
+  test("Pass", () => {
+    expect(true).toEqual(true);
+  });
+});
